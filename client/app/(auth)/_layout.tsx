8cea3e7acdc0e496f4c704b00f8c227210f9bf2c
Remxin <remxin7@gmail.com>
2024-08-28 18:27:50 +0200

    feat(client): get family page design

--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -1,26 +1,32 @@
-import { Stack } from 'expo-router';
-import React from 'react';
+import { Stack } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
 
 export default function TabLayout() {
-
   return (
     <Stack
       screenOptions={{
         headerShown: false,
-      }}>
+      }}
+    >
       <Stack.Screen
         name="index"
         options={{
-          title: 'login',
+          title: "login",
         }}
       />
       <Stack.Screen
         name="register"
         options={{
-            title: "register",
+          title: "register",
+        }}
+      />
+      <Stack.Screen
+        name="getfamily"
+        options={{
+          title: "get family",
         }}
       />
     </Stack>
--- client/validations/val.ts
+++ client/validations/val.ts
@@ -1,5 +1,5 @@
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-const usernameRegex = /`^[a-zA-Z\s]+$`/
+const usernameRegex = /^[a-zA-Z\s]+$/
 
 export function validateString(value: string, min: number, max: number): string {
     if (value.length < min || value.length > max) return `should have length between ${min} and ${max}`
